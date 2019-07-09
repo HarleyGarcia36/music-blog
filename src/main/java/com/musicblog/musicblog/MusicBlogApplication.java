530ce32c7a63ec47e9af41f69923611a02c91fcc
Harley Garcia <orion@Harleys-MacBook-Air.local>
2019-07-09 08:47:09 -0500

    first commit

---
+++ src/main/java/com/musicblog/musicblog/MusicBlogApplication.java
@@ -0,0 +1,13 @@
+package com.musicblog.musicblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicBlogApplication.class, args);
+    }
+
+}
